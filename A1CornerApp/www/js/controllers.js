870cef1ef701f2e0086412981faaa7a680d991b9
Maxime ANDRÉ <maxime.andre@eleve.enac.fr>
2015-11-19 23:17:43 +0100

    test of some services working great

--- A1CornerApp/www/js/controllers.js
+++ A1CornerApp/www/js/controllers.js
@@ -1,6 +1,51 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $cordovaToast, $cordovaDeviceMotion, $cordovaVibration) {
+  $scope.toto = "toto";
+
+document.addEventListener("deviceready", function () {
+   $cordovaVibration.vibrate(10000);
+  $cordovaToast.show('Here is a message', 'long', 'center')
+    .then(function(success) {
+      // success
+    }, function (error) {
+      // error
+    });
+}, false);
+
+
+  var options = { frequency: 2000 };
+
+  document.addEventListener("deviceready", function () {
+
+
+    var watch = $cordovaDeviceMotion.watchAcceleration(options);
+    watch.then(
+      null,
+      function(error) {
+      // An error occurred
+      },
+      function(result) {
+        $scope._X = result.x;
+        $scope._Y = result.y;
+        $scope._Z = result.z;
+        var timeStamp = result.timestamp;
+    });
+
+
+    watch.clearWatch();
+    // OR
+    $cordovaDeviceMotion.clearWatch(watch)
+      .then(function(result) {
+        // success
+        }, function (error) {
+        // error
+      });
+
+  }, false);
+
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
@@ -10,6 +55,10 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+
+  
+  console.log('coucou');
+
 
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
